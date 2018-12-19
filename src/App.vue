4ce14da31c8c5f223e6003dc4be95e1e7f4d1ce5
Andrey Aratov <madingame@yandex.ru>
2018-12-19 22:52:13 +0300

    list item component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <ol>
+            <List v-for="item in todos" v-bind:items="item"/>
+        </ol>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import List from './components/List.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            List
+        },
+        data() {
+            return {
+                todos: [
+                    {
+                        title: "title 1"
+                    },
+                    {
+                        title: "title 2"
+                    },
+                    {
+                        title: "title 3"
+                    }
+                ]
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .test {
+        color: red;
+    }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,10 @@
+<template>
+    <li>{{ items.title }}</li>
+</template>
+
+<script>
+    export default {
+        name: 'List',
+        props: ['items']
+    }
+</script>
