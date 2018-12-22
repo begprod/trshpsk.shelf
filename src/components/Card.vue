8cc504afcfd413eae158d4e540532b820ac77743
Andrey Aratov <madingame@yandex.ru>
2018-12-22 14:21:09 +0300

    added icons

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,25 @@
 <template>
     <div class="card">
         <div class="card__inner">
-            <h2 class="card__title">{{ items.title }}</h2>
-            <h3 class="card__title card__title_md">{{ items.title_eng }}</h3>
-            <h4 class="card__title card__title_sm">{{ items.year }}</h4>
+            <div class="card__info">
+                <h2 class="card__title">{{ items.title }}</h2>
+                <h3 class="card__title card__title_md" v-if="items.title_eng">{{ items.title_eng }}</h3>
+                <h4 class="card__title card__title_sm">{{ items.year }}</h4>
+            </div>
+            <div class="card__status">
+                <svg class="card__icon">
+                    <use xlink:href="./img/icons-svg/orion-svg-sprite.svg#checkbox-confirm-circle-1"></use>
+                </svg>
+                <svg class="card__icon">
+                    <use xlink:href="./img/icons-svg/orion-svg-sprite.svg#video-camera-1"></use>
+                </svg>
+                <svg class="card__icon">
+                    <use xlink:href="./img/icons-svg/orion-svg-sprite.svg#save-file-1"></use>
+                </svg>
+                <svg class="card__icon">
+                    <use xlink:href="./img/icons-svg/orion-svg-sprite.svg#verify-woman-user-1"></use>
+                </svg>
+            </div>
         </div>
     </div>
 </template>
@@ -17,6 +33,8 @@
 
 <style lang="scss">
     .card {
+        display: flex;
+        align-items: stretch;
         margin-right: 25px;
         width: 300px;
         height: 300px;
@@ -34,6 +52,10 @@
             margin-top: 25px;
         }
         &__inner {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            width: 100%;
             padding: 20px;
         }
         &__title {
@@ -51,6 +73,14 @@
             &_sm {
                 font-size: 14px;
                 color: rgba(75, 75, 75, .6);
+            }
+        }
+        &__icon {
+            width: 35px;
+            height: 35px;
+            margin-right: 15px;
+            &:last-child {
+                margin-right: 0;
             }
         }
     }
