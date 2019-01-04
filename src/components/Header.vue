8598041bb9e29192b41c213884d4a7082fef90c7
Andrey Aratov <madingame@yandex.ru>
2019-01-04 15:23:25 +0300

    Header component / 3

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,12 @@
 <template>
     <header class="header">
-        <div class="header__logo">
-            <slot name="header-name"></slot>
-        </div>
-        <div class="header__search">
-            <Search v-model="searchValue" v-on:input="$emit('input', searchValue)" placeholder="Поиск"/>
+        <div class="wrapper header__inner">
+            <div class="header__logo">
+                <slot name="header-name"></slot>
+            </div>
+            <div class="header__search">
+                <Search v-model="searchValue" v-on:input="$emit('input', searchValue)" placeholder="Поиск"/>
+            </div>
         </div>
     </header>
 </template>
@@ -27,12 +29,15 @@
 
 <style lang="scss">
     .header {
-        display: flex;
-        justify-content: center;
-        align-items: center;
         margin-bottom: 70px;
         height: 70px;
         background-color: #1f1f1f;
+        &__inner {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 100%;
+        }
         &__logo {
             margin-right: 70px;
             font-size: 28px;
@@ -40,6 +45,7 @@
             color: white;
         }
         &__search {
+            flex-grow: 1;
             width: 50%;
             height: 40px;
         }
